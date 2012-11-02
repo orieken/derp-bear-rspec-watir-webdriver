9ad84d14b19f6b42ae984d4718b07f74655a33ea
Oscar Rieken <oriekenjr@gmail.com>
2012-11-01 23:48:12 -0400

    added some context around the simple tests its a bit verbose but for now they are all in one spec each

--- spec/isolation/derp_bear/taza_watir_webdriver_spec.rb
+++ spec/isolation/derp_bear/taza_watir_webdriver_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 require 'derp_bear'
 
-describe "Validating Derp Bear home page using taza page objects" do
-  let(:derp_bear){ DerpBear.new }
+describe "Rspec, Watir-WebDriver, and Taza Page Objects" do
+  let(:derp_bear) { DerpBear.new }
 
   after(:each) do
     derp_bear.close
   end
 
-  context "title" do
-    it { derp_bear.home_page.title.should eql "Welcome to Derp-Bear" }
+  context "Validating Derp Bear home page" do
+    context "title" do
+      it { derp_bear.home_page.title.should eql "Welcome to Derp-Bear" }
+    end
   end
 end
--- spec/isolation/derp_bear/watir_webdriver_spec.rb
+++ spec/isolation/derp_bear/watir_webdriver_spec.rb
@@ -1,19 +1,41 @@
 require 'spec_helper'
 require 'derp_bear'
 
-describe "Validating Derp Bear" do
-  let(:browser){ Watir::Browser.new :chrome }
+describe "RSpec and Watir-WebDriver" do
+  let(:browser) { Watir::Browser.new :chrome }
 
   before(:each) do
     browser.goto "http://derp-bear.herokuapp.com/"
   end
 
   after(:each) do
-      browser.close
+    browser.close
   end
 
-  context "home page"
-  it "should have the correct title" do
-    browser.title.should eql "Welcome to Derp-Bear"
-  end
-end
+  context "Derp Bear" do
+    context "home page" do
+      it "should have the correct title" do
+        browser.title.should eql "Welcome to Derp-Bear"
+      end
+
+      context "Nav area" do
+        it "should have a nav section" do
+          browser.nav(id: "primary").should be_present
+        end
+
+        it "should have a Home link by going down the tree" do
+          browser.nav.ul.li.link.text.should eql "HOME"
+        end
+
+        it "should have a Home link by id" do
+          browser.link(id: "home_link").should be_present
+        end
+
+        it "should have a Home link by Text" do
+          browser.link(text: "Home").should be_present
+        end
+
+      end
+    end #home_page
+  end #derp_bear
+end #describe
